508f1e15e0714ffc00829a9112a219723f4e5f39
damonlin <damonalbarn0519@gmail.com>
2019-06-25 14:27:57 +0800

    Day06_SetupKernelStack

---
+++ Day06_SetupKernelStack/init/main.c
@@ -0,0 +1,15 @@
+
+// for kernel's stack
+long kernel_stack[1024] ;
+
+struct {
+	long * esp;	// esp
+	short ss;	// ss in 0x10(data segment)
+} stack_start = { &kernel_stack[1024] , 0x10 };
+
+void show_msg();
+
+void main(void)
+{
+        show_msg();        
+}
