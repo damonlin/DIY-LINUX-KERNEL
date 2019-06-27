54efd0eacdcf9bb233db7650b3e85fe3d2223480
damonlin <damonalbarn0519@gmail.com>
2019-06-27 11:01:08 +0800

    Day09_Printk

---
+++ Day09_Printk/README.md
@@ -0,0 +1,23 @@
+# 第八天
+
+實做 printk. 新增可以格式化字串.
+
+## 程式流程
+
+1. vsprintf.c : 
+
+* 根據參數 format 字串轉換並格式化數據
+
+2. printk.c : 
+
+* 主要用於 Kernel 輸出字串, 先利用 `vsprintf()` 格式化字串, 再交給 `con_write()` 輸出.
+
+## 編譯
+```bash
+make
+```
+
+## 執行
+```bash
+make start
+```
---
+++ Day09_Printk/init/main.c
@@ -0,0 +1,19 @@
+#include <linux/kernel.h>
+
+// for kernel's stack
+long kernel_stack[1024] ;
+
+struct {
+	long * esp;	// esp
+	short ss;	// ss in 0x10(data segment)
+} stack_start = { &kernel_stack[1024] , 0x10 };
+
+extern void con_init();
+
+void main(void)
+{
+	con_init();
+	
+	printk("Entering kernel ...\n");
+	printk("%s %d\n", "This year is ", 2019 );
+}
