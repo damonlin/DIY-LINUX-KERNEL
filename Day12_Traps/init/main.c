e06c40bddb1488ccbaea2089f4ab8b638126b4a0
damonlin <damonalbarn0519@gmail.com>
2019-07-01 14:00:07 +0800

    Move kernel_stack to sched.c

--- Day12_Traps/init/main.c
+++ Day12_Traps/init/main.c
@@ -1,14 +1,6 @@
 #include <linux/kernel.h>
 #include <asm/system.h>
 
-// for kernel's stack
-long kernel_stack[1024] ;
-
-struct {
-	long * esp;	// esp
-	short ss;	// ss in 0x10(data segment)
-} stack_start = { &kernel_stack[1024] , 0x10 };
-
 extern void con_init();
 extern void timer_init();
 extern void trap_init();
--- Day12_Traps/kernel/sched.c
+++ Day12_Traps/kernel/sched.c
@@ -7,6 +7,14 @@
 
 extern int timer_interrupt(void);
 
+// for kernel's stack
+long kernel_stack[1024] ;
+
+struct {
+	long * esp;	// esp
+	short ss;	// ss in 0x10(data segment)
+} stack_start = { &kernel_stack[1024] , 0x10 };
+
 static long jiffies = 0;
 
 void do_timer()
